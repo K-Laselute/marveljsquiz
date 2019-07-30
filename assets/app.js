69417aa3867c47c7b08a3409f872fa02b57ba5d3
Kyle Laselute <klaselute@gmail.com>
2019-07-29 23:33:12 -0600

    Changes to html, styling, and reorganized js. Modifying slide animation

--- assets/app.js
+++ assets/app.js
@@ -1,90 +1,4 @@
-const difficulty = document.querySelector('.difficulty');
-let easy = document.querySelector('#easy');
-let medium = document.querySelector('#medium');
-let hard = document.querySelector('#hard');;
-let submit = document.querySelector('#submit');
-submit.style.display = 'none';
-
-easy.addEventListener('click', function () {
-    easyQuiz();
-    submit.style.display = 'inline';
-    difficulty.style.display = 'none';
-});
-
-medium.addEventListener('click', function () {
-    mediumQuiz();
-    submit.style.display = 'inline';
-    difficulty.style.display = 'none';
-});
-
-function easyQuiz() {
-    function buildQuiz() {
-        // we'll need a place to store the HTML output
-        const output = [];
-
-        // for each question...
-        myQuestions.forEach((currentQuestion, questionNumber) => {
-            // we'll want to store the list of answer choices
-            const answers = [];
-
-            // and for each available answer...
-            for (letter in currentQuestion.answers) {
-                // ...add an HTML radio button
-                answers.push(
-                    `<label>
-        <input type="radio" name="question${questionNumber}" value="${letter}">
-        ${letter} :
-        ${currentQuestion.answers[letter]}
-        </label>`
-                );
-            }
-
-            // add this question and its answers to the output
-            output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-    <div class="answers"> ${answers.join(" ")} </div>`
-            );
-        });
-
-        // finally combine our output list into one string of HTML and put it on the page
-        quizContainer.innerHTML = output.join("");
-    }
-
-    function showResults() {
-        // gather answer containers from our quiz
-        const answerContainers = quizContainer.querySelectorAll(".answers");
-
-        // keep track of user's answers
-        let numCorrect = 0;
-
-        // for each question...
-        myQuestions.forEach((currentQuestion, questionNumber) => {
-            // find selected answer
-            const answerContainer = answerContainers[questionNumber];
-            const selector = `input[name=question${questionNumber}]:checked`;
-            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-            // if answer is correct
-            if (userAnswer === currentQuestion.correctAnswer) {
-                // add to the number of correct answers
-                numCorrect++;
-
-                // color the answers green
-                answerContainers[questionNumber].style.color = "lightgreen";
-            } else {
-                // if answer is wrong or blank
-                // color the answers red
-                answerContainers[questionNumber].style.color = "red";
-            }
-        });
-
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
-
-    const quizContainer = document.getElementById("quiz");
-    const resultsContainer = document.getElementById("results");
-    const submitButton = document.getElementById("submit");
+(function () {
     const myQuestions = [
         {
             question: "Who was not part of the original X-Men?",
@@ -138,14 +52,6 @@ function easyQuiz() {
         }
     ];
 
-    // display quiz right away
-    buildQuiz();
-
-    // on submit, show results
-    submitButton.addEventListener("click", showResults);
-};
-
-function mediumQuiz() {
     function buildQuiz() {
         // we'll need a place to store the HTML output
         const output = [];
@@ -160,17 +66,19 @@ function mediumQuiz() {
                 // ...add an HTML radio button
                 answers.push(
                     `<label>
-        <input type="radio" name="question${questionNumber}" value="${letter}">
-        ${letter} :
-        ${currentQuestion.answers[letter]}
-        </label>`
+                        <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
                 );
             }
 
             // add this question and its answers to the output
             output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-    <div class="answers"> ${answers.join(" ")} </div>`
+                `<div class="slide">
+                    <div class="question"> ${currentQuestion.question} </div>
+                    <div class="answers"> ${answers.join("")} </div>
+                </div>`
             );
         });
 
@@ -208,67 +116,54 @@ function mediumQuiz() {
 
         // show number of correct answers out of total
         resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
+    };
+
+    //slide animation
+    function showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+
+        if (currentSlide === 0) {
+            previousButton.style.display = 'none';
+        } else {
+            previousButton.style.display = 'inline-block';
+        }
+
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        } else {
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
+    };
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    };
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    };
 
     const quizContainer = document.getElementById("quiz");
     const resultsContainer = document.getElementById("results");
     const submitButton = document.getElementById("submit");
-    const myQuestions = [
-        {
-            question: "The Vision is an Android created by?",
-            answers: {
-                a: "Reed Richards",
-                b: "Tony Stark",
-                c: "Ultron",
-                d: "Doctor Doom"
-            },
-            correctAnswer: "c"
-        },
-        {
-            question: "How many rings of power does Mandarin possess?",
-            answers: {
-                a: "1",
-                b: "2",
-                c: "3",
-                d: "10"
-            },
-            correctAnswer: "d"
-        },
-        {
-            question: "Ghost Rider is known as?",
-            answers: {
-                a: "The Guardian Devil",
-                b: "The Spirit of Hate",
-                c: "The Spirit of Vengenance",
-                d: "The Red Skull"
-            },
-            correctAnswer: "c"
-        },
-        {
-            question: "The Fantastic Four have the headquarters in what building?",
-            answers: {
-                a: "Stark Tower",
-                b: "Fantastic Headquarters",
-                c: "Baxter Building",
-                d: "Xavier Institute"
-            },
-            correctAnswer: "c"
-        },
-        {
-            question: "Who paid Mac Gargan to become the Scorpion?",
-            answers: {
-                a: "Felicia Hardy",
-                b: "The Kingpin",
-                c: "Norman Osborn",
-                d: "J. Jonah Jameson"
-            },
-            correctAnswer: "d"
-        }
-    ];
 
     // display quiz right away
     buildQuiz();
 
+    //pagination
+    const previousButton = document.getElementById('previous');
+    const nextButton = document.getElementById('next');
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+
+    showSlide(0);
+
     // on submit, show results
     submitButton.addEventListener("click", showResults);
-};
+    previousButton.addEventListener('click', showPreviousSlide);
+    nextButton.addEventListener('click', showNextSlide); 
+})();
