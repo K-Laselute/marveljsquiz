7c9b10f7dae8eaea2d0b8ad3535c1ca66ae7ceba
Kyle Laselute <klaselute@gmail.com>
2019-08-27 00:38:43 -0600

    Started app from scratch. New html. New styling.

--- assets/app.js
+++ assets/app.js
@@ -1,52 +1,103 @@
-const myQuestions = [
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
     {
         question: "Who was not part of the original X-Men?",
-        answers: {
-            a: "Storm",
-            b: "Cyclops",
-            c: "Iceman",
-            d: "Beast"
-        },
-        correctAnswer: "a"
+        choice1: "Storm",
+        choice2: "Cyclops",
+        choice3: "Iceman",
+        choice4: "Beast",
+        answer: 1
     },
     {
         question: "Where does Dr. Strange seek out a cure for his damaged hands?",
-        answers: {
-            a: "China",
-            b: "Tibet",
-            c: "Mongolia",
-            d: "New York"
-        },
-        correctAnswer: "b"
+        choice1: "China",
+        choice2: "Tibet",
+        choice3: "Mongolia",
+        choice4: "New York",
+        answer: 2
     },
     {
         question: "How old is thor",
-        answers: {
-            a: "100",
-            b: "500",
-            c: "1000",
-            d: "1500"
-        },
-        correctAnswer: "c"
+        choice1: "100",
+        choice2: "500",
+        choice3: "1000",
+        choice4: "1500",
+        answer: 3
     },
     {
         question: "The comic book version of 'Iron Man' is based on what real-life eccentric?",
-        answers: {
-            a: "Warren Beatty",
-            b: "Elon Musk",
-            c: "Howard Hughes",
-            d: "Stan Lee"
-        },
-        correctAnswer: "c"
+        choice1: "Warren Beatty",
+        choice2: "Elon Musk",
+        choice3: "Howard Hughes",
+        choice4: "Stan Lee",
+        answer: 3
     },
     {
         question: "Vanessa Marianna becomes the wife of this Marvel supervillain",
-        answers: {
-            a: "Dr. Octopus",
-            b: "Kingpin",
-            c: "Magneto",
-            d: "Dr. Doom"
-        },
-        correctAnswer: "b"
+        choice1: "Dr. Octopus",
+        choice2: "Kingpin",
+        choice3: "Magneto",
+        choice4: "Dr. Doom",
+        answer: 2
     }
 ];
+
+// CONSTANTS
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+
+    // No more questions? 
+    if(availableQuestions === 0 || questionCounter > MAX_QUESTIONS) {
+        // go to the end page
+        return window.location.assign(*/end.html*);
+    }
+    // Generate a random question and assign it to the h2 w/ class of question 
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    // grab each choice from the available question and add them to the page
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+    // remove question from list
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
+
+// Add click listeners for each choice
+choices.forEach( choice => {
+    choice.addEventListener('click', e => {
+        // If we are not accepting answers
+        if(!acceptingAnswers) return;
+        // 
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        console.log(selectedAnswer);
+        getNewQuestion();
+    });
+});
+
+startGame();
